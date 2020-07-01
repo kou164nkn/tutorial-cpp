000a40befe7613bd6d5b611293c26625c9cef386
kou164nkn <galahad1005@gmail.com>
2020-07-01 23:33:16 +0900

    ボタンを押したらウィンドウを閉じる機能

--- tutorial_qt/main.cpp
+++ tutorial_qt/main.cpp
@@ -8,10 +8,6 @@ int main(int argc, char **argv)
   QApplication app (argc, argv);
 
   Window window;
-  window.setFixedSize(300, 200);
-
-  QPushButton *button = new QPushButton("Hello World", &window);
-  button->setGeometry(10, 10, 120, 50);
 
   window.show();
   return app.exec();
--- tutorial_qt/window.cpp
+++ tutorial_qt/window.cpp
@@ -1,12 +1,19 @@
 #include "window.hpp"
+
+#include <QApplication>
 #include <QPushButton>
 
 Window::Window(QWidget *parent) :
 QWidget(parent){
   // Set size of the window
-  // setFixedSize(100, 50);
+  setFixedSize(100, 50);
 
   // Create and position the button
   button_ = new QPushButton("2nd button", this);
-  button_->setGeometry(10, 10, 100, 30);
+  button_->setGeometry(10, 10, 80, 30);
+
+  connect(button_,
+          SIGNAL(clicked()),
+          QApplication::instance(),
+          SLOT(quit()));
 }
